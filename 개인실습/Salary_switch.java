1f8d3f430ffdf745e8cfb415deb6dc9e091aa1e9
hayeong <jbg01200249@gmail.com>
2023-05-28 17:11:48 +0900

    Switch문 한글오류잡는 법

--- 개인실습/Salary_switch.java
+++ 개인실습/Salary_switch.java
@@ -4,41 +4,40 @@ class Salary_Switch
 {
 	public static void main(String args[])
 	{
-		Scanner sc = new Scanner(System.in);
-		double currentSalary, newSalary, raise = 0;	//현 연봉, 새 연봉, 연봉 인상액
-		String rating;		//근무 평급
+		Scanner sc = new Scanner(System.in, "EUC-KR");
+		double currentSalary, newSalary, raise;	//현 연봉, 새 연봉, 연봉 인상액
+		String rating;		//근무 평가등급
 
 		System.out.print("현 연봉을 입력하세요 : ");
-		currentSalary = sc.nextDouble();
+		currentSalary = sc.nextDouble();		
 
 		System.out.print("근무평가등급을 입력하세요 : ");
 		rating = sc.next();
+		System.out.println("입력한 근무평가 등급은 " + rating + "입니다.");
 
 		switch(rating)
 		{
 			case "우수":
-			raise = currentSalary*0.6;
-			System.out.println("연봉인상액 : " + raise);
-       		newSalary = currentSalary + raise;
-			System.out.println("새 연봉인상액 : " + newSalary);
-			break;
+				raise = currentSalary*0.06;
+				System.out.println("연봉인상액 : " + (int)raise);
+       			newSalary = currentSalary + raise;
+				System.out.println("새 연봉인상액 : " + (int)newSalary);
+				break;
 
 			case "보통":
-			raise = currentSalary*0.4;
-			System.out.println("연봉인상액 : " + raise);
-       		newSalary = currentSalary + raise;
-			System.out.println("새 연봉인상액 : " + newSalary);
-			break;
+				raise = currentSalary*0.04;
+				System.out.println("연봉인상액 : " + (int)raise);
+       			newSalary = currentSalary + raise;
+				System.out.println("새 연봉인상액 : " + (int)newSalary);
+				break;
 
 			case "불량":
-			raise = currentSalary*0.2;
-			System.out.println("연봉인상액 : " + raise);
-       		newSalary = currentSalary + raise;
-			System.out.println("새 연봉인상액 : " + newSalary);
-			break;
+				raise = currentSalary*0.02;
+				System.out.println("연봉인상액 : " + (int)raise);
+       			newSalary = currentSalary + raise;
+				System.out.println("새 연봉인상액 : " + (int)newSalary);
+				break;
 		}
-
-			
 			
 	}
 }
