ab368806320f79255f4fea50c368d7ae4a1f5418
hayeong <jbg01200249@gmail.com>
2023-05-31 18:28:31 +0900

    입력받은 문자열을 출력하는 프로그램

--- 개인실습/SwitchOp.java
+++ 개인실습/SwitchOp.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-class SwitchExam1
+class SwitchOp
 {
 	public static void main(String args[])
 	{
 		int num1, num2;
-		String arithmetic;
+		String op;
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("수식 입력(수와 연산자 사이는 공백을 둘 것) : ");
 		num1 = sc.nextInt();
-		arithmetic = sc.next();
+		op = sc.next();
 		num2 = sc.nextInt();
 
-		switch(arithmetic)
+		switch(op)
 		{
 			case "+":
 				System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
