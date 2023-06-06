38256a5c5b33ddf2a6e5b5bdcaa66c6c249af125
hayeong <jbg01200249@gmail.com>
2023-06-06 17:18:56 +0900

    10개의 정수를 입력받아 3 또는 5의 배수를 구하는 프로그램

--- 개인실습/ForTest6.java
+++ 개인실습/ForTest6.java
@@ -4,27 +4,21 @@ class ForTest6
 {
 	public static void main(String args[])
 	{
-		int n1, n2, count = 0;
+		int n, count = 0;
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("정수 10개를 입력하세요:");
-		n1 = sc.nextInt(); n2 = sc.nextInt();
-		n3 = sc.nextInt(); n4 = sc.nextInt(); 
-		n5 = sc.nextInt(); n6 = sc.nextInt();
-		n7 = sc.nextInt(); n8 = sc.nextInt();
-		n9 = sc.nextInt(); n10 = sc.nextInt();
 
-		
-
-		for(int i=0; i<11; i++)
+		for(int i=0; i<10; i++)
  		{
-			if(n1%3 == 0 || n1%5 == 0)
+			n = sc.nextInt();
+
+			if(n % 3 == 0 || n % 5 == 0)
+			{
 				count = count + 1;
-			if(n2%3 == 0 || n2%5 == 0)
-				count = count + 1;
-			
+			}
 		}
 		
-		System.out.println("3또는 5의 배수의 개수 : " + count);
+		System.out.println("3 또는 5의 배수의 개수 : " + count);
 	}
 }
