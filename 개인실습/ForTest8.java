7a9a6627249c80bb769df7ffab4e7ce9682a6e8e
hayeong120 <jbg01200249@gmail.com>
2023-07-10 16:39:16 +0900

    5행 10열로 별을 찍어보자

---
+++ 개인실습/ForTest8.java
@@ -0,0 +1,14 @@
+class ForTest8
+{
+	public static void main(String args[])
+	{
+		for (int i=1; i<=5; i++)
+		{
+			for (int j=1; j<=11; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
