0bdceb3b9e90b6ff87fb15258889d4e1ad194ecc
장하영 <129273224+hayeong120@users.noreply.github.com>
2025-02-25 02:48:10 +0900

    [level 1] Title: 예산, Time: 0.56 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,12 +3,13 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
+        int tmp = 0;
         
         Arrays.sort(d);
         
         for(int a : d) {
-            budget -= a;
-            if(budget < 0) break;
+            tmp += a;
+            if(budget < tmp) break;
             answer++;            
         }
         
